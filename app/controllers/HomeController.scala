a3d711e2bb075c344e024eb4fcc69084343a6f0f
Ayaz25 <ayazhasan@web.de>
2022-11-29 10:12:19 +0100

    Ajex

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,6 +6,7 @@ import com.google.inject.{Guice, Injector}
 import de.htwg.se.schwimmen.schwimmenModul
 import de.htwg.se.schwimmen.controller.controllerComponent.ControllerInterface
 import de.htwg.se.schwimmen.aUI.TUI
+import play.api.libs.json.{JsValue, JsObject, Json, Writes}
 
 import javax.inject._
 /**
@@ -37,51 +38,118 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
     Ok(views.html.game(this))
   }
 
-  /**
-   * Puts tile of given color in specified slot
-   * @param input input as String
-   * @return
-   */
-  def setPlayerAmount(count: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    //controller.setPlayerAmount(count.toInt)
-    Redirect("/playerCount")
-    tui.input = count
-    tui.processInput()
-    Ok(views.html.game(this))
+  def setPlayerAmount(count: String) = {
+    controller.setPlayerAmount(count.toInt)
   }
 
-  def setPlayerName(name: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Redirect("/playerName")
-    tui.input = name
-    tui.processInput()
-    Ok(views.html.game(this))
-  }
-
-  def tackAllcards(ev: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Redirect("/tackAll")
-    tui.input = ev
-    tui.processInput()
-    Ok(views.html.game(this))
-  }
-
-  def changeOnCard(ev: String, handcard: Int, fieldcard: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Redirect("/change")
-    if (ev == "y" && handcard  != -1 && fieldcard != -1) {
-      tui.input = ev
-      tui.processInput()
-      tui.input = handcard.toString
-      tui.processInput()
-      tui.input = fieldcard.toString
-      tui.processInput()
+    def setPlayerName(name: String) = {
+      controller.addPlayer(name)
     }
-    Ok(views.html.game(this))
+
+
+      def changeAllCards() = {
+        controller.swapAllCards()
+        controller.nextPlayer()
+      }
+
+      def knock() = {
+        controller.setKnocked()
+        controller.nextPlayer()
+      }
+
+      def changeOneCard(handCard: Int, fieldCard: Int) = {
+        if (handCard != -1 && fieldCard != -1) {
+          controller.swapCards(handCard, fieldCard)
+          controller.nextPlayer()
+        }
+      }
+
+      def setNextRound() = {
+        controller.nextRound()
+      }
+
+      def undo() = {
+        controller.undo()
+      }
+
+      def redo() = {
+        controller.redo()
+      }
+
+      def saveGame() = {
+        controller.saveTo("saveJson")
+      }
+
+      def loadGame() = {
+        controller.loadFrom("loadJson")
+      }
+
+      def gameProcessComand(comand: String,  data: String): String = {
+        if (comand.equals("\"all\"")) {
+          changeAllCards()
+        } else if (comand.equals("\"amount\"")) {
+          setPlayerAmount(data.replace("\"", ""))
+        } else if (comand.equals("\"addplayer\"")) {
+          setPlayerName(data.replace("\"", ""))
+        } else if (comand.equals("\"k\"")) {
+          knock()
+        } else if (comand.equals("\"nr\"")) {
+          setNextRound()
+        } else if (comand.equals("\"undo\"")) {
+          undo()
+        } else if (comand.equals("\"redo\"")) {
+          redo()
+        } else if (comand.equals("\"save\"")) {
+          saveGame()
+        } else if (comand.equals("\"load\"")) {
+          loadGame()
+        }
+        "Ok"
+      }
+
+  case class Gamefield()
+  implicit val gamefieldWrites: Writes[Gamefield] = new Writes[Gamefield] {
+    def writes(gamefield: Gamefield): JsValue = Json.toJson(
+      for {
+        player <- 0 until controller.playerAmount
+      } yield {
+        Json.obj(
+          "player_cards_1" -> controller.players(player).cardsOnHand.head,
+          "player_cards_2" -> controller.players(player).cardsOnHand(1),
+          "player_cards_3" -> controller.players(player).cardsOnHand.last,
+          "field_card_1" -> controller.field.cardsOnField.head,
+          "field_card_2" -> controller.field.cardsOnField(1),
+          "field_card_3" -> controller.field.cardsOnField.last,
+        )
+      }
+    )
   }
 
-  def nextRound(ev: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Redirect("/nextRound")
-    tui.input = ev
-    tui.processInput()
-    Ok(views.html.game(this))
-  }
+      def status = Action {
+        Ok(Json.obj(
+          "gamefield" -> Gamefield(),
+          "game_status" -> tui.getGameState()
+          )
+        )
+      }
+
+      def gameRequest = Action {
+        implicit request => {
+          val req = request.body.asJson
+          gameProcessComand(req.get("cmd").toString(), req.get("data").toString())
+          Ok(Json.obj(
+            "gamefield" -> Gamefield(),
+            "game_status" -> tui.getGameState()
+            )
+          )
+        }
+      }
+
+      def allRoutes: String = {
+        """
+       GET  /
+       GET  /command
+    """
+      }
 
 }
